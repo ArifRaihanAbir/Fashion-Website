df418402e14731d7119922e7d0f8d76572d49aad
Tahmina Ferdous <72664181+ArifRaihanAbir@users.noreply.github.com>
2022-04-14 04:16:38 +0600

    webste

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// small screen menubar
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if(bar){
+    bar.addEventListener('click',()=>{
+        nav.classList.add('active');
+    })
+}
+if(close){
+    close.addEventListener('click',()=>{
+        nav.classList.remove('active');
+    })
+}
+
+// single product details
+var mainimg =document.getElementById("mainid");
+var smallimg =document.getElementsByClassName("small-img");
+
+smallimg[0].onclick = function(){
+    mainimg.src= smallimg[0].src;
+}
+smallimg[1].onclick = function(){
+    mainimg.src= smallimg[1].src;
+}
+smallimg[2].onclick = function(){
+    mainimg.src= smallimg[2].src;
+}
+smallimg[3].onclick = function(){
+    mainimg.src= smallimg[3].src;
+}
